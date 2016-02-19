16502ebbf490efc77bb27807c3e2a996369499bd
Shawn Waldon <shawn.waldon@kitware.com>
2016-02-19 15:00:19 -0500

    refactor(LightViz): Use the new paraviewweb module rather than the various tonic modules
    Replace tonic-ui and tonic-io with the corresponding components in ParaViewWeb

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+var path = require('path'),
+    loaders = require('./node_modules/paraviewweb/config/webpack.loaders.js');
+
 module.exports = {
   plugins: [],
   entry: './lib/app.js',
@@ -13,16 +16,13 @@ module.exports = {
         }],
     loaders: [
       { test: require.resolve("./lib/app.js"), loader: "expose?LightViz" },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=60000&mimetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=60000" },
-      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
-      { test: /\.css$/, loader: "style-loader!css-loader!autoprefixer-loader?browsers=last 2 version" },
-      { test: /\.c$/i, loader: "shader" },
-      { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.js$/, include: /node_modules\/tonic-/, loader: "babel?presets[]=react,presets[]=es2015" },
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel?presets[]=react,presets[]=es2015" },
-    ],
+    ].concat(loaders),
   },
+  resolve: {
+        alias: {
+            PVWStyle: path.resolve('./node_modules/paraviewweb/style'),
+        },
+    },
   eslint: {
         configFile: '.eslintrc',
   },

461848c0fed051b9958edad5910c6904bba89dd5
Shawn Waldon <shawn.waldon@kitware.com>
2016-05-16 13:34:34 -0600

    docs(Documentation): Added initial version of docs.  Still needs to be fleshed out.

---
+++ Documentation/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  cname: 'kitware.github.io',
+  baseUrl: '/lightviz',
+  work: './build-tmp',
+  config: {
+    title: 'LightViz.js',
+    description: '"Visualization Toolkit for the Web."',
+    subtitle: '"Enable scientific visualization to the Web."',
+    author: 'Kitware Inc.',
+    timezone: 'UTC',
+    url: 'https://kitware.github.io/lightviz',
+    root: '/lightviz/',
+    github: 'kitware/lightviz',
+  }
+};
---
+++ Documentation/content/docs/index.md
@@ -0,0 +1,6 @@
+LightViz
+--------
+
+LightViz is a simple paraviewweb-based application for viewing datasets.
+
+

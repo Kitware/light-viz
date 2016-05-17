00d12b569eaf90ab395650a70e6ba9cef9727b88
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2016-05-17 09:39:43 -0600

    style(SVG): Add svg icons

---
+++ svg/index.js
@@ -0,0 +1,19 @@
+import Clip            from './Clip.svg';
+import Contour         from './Contour.svg';
+import DataSet         from './DataSet.svg';
+import LightViz        from './LightViz.svg';
+import SliceOrthogonal from './SliceOrthogonal.svg';
+import SliceParallel   from './SliceParallel.svg';
+import StreamLines     from './StreamLines.svg';
+import Volume          from './Volume.svg';
+
+export default {
+  Clip,
+  Contour,
+  DataSet,
+  LightViz,
+  SliceOrthogonal,
+  SliceParallel,
+  StreamLines,
+  Volume,
+};

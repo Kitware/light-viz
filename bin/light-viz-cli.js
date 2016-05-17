d73fe157360e0af959e5c8f7000bba6cf5591026
Shawn Waldon <shawn.waldon@kitware.com>
2016-05-17 17:21:00 -0600

    feat(AddDataset): Add an option on the Lightviz executable to add a dataset to the data dir

--- bin/light-viz-cli.js
+++ bin/light-viz-cli.js
@@ -18,10 +18,13 @@ program
   .option('--data-analysis', 'Inspect data directory and compute metadata\n')
 
   .option('--config [path]', 'Provide a Lightviz config file to use')
-  .option('--profile [profile]', 'Specify which profile from the config file to use')
+  .option('--profile [profile]', 'Specify which profile from the config file to use\n')
+
+  .option('--add-dataset [path]', 'Specify a dataset to add to the given data directory.  Requires the description and data flags')
+  .option('--description [description]', 'Specify the description for the dataset being added')
+  .option('--autoApply', 'Optional for use with --add-dataset.  Specifies that apply/reset buttons are not needed with the dataset')
 
   .parse(process.argv);
-
 
 if(!paraview) {
     paraview = [];
@@ -41,42 +44,75 @@ if (!process.argv.slice(2).length) {
     process.exit(0);
 }
 
+
 var pvPythonExecs = find(paraview).filter(function(file) { return file.match(/pvpython$/) || file.match(/pvpython.exe$/); });
 if(pvPythonExecs.length < 1) {
     console.log('Could not find pvpython in your ParaView HOME directory ($PARAVIEW_HOME)');
     program.outputHelp();
 } else {
-    const cmdLine = [
-        pvPythonExecs[0], '-dr',
-        path.normalize(path.join(__dirname, '../server/pvw-light-viz.py')),
-        '--content', path.normalize(path.join(__dirname, '../dist')),
-        '--port', program.port,
-    ];
-    if (program.data) {
-      cmdLine.push('--data');
-      cmdLine.push(program.data);
-    }
-    if (program.config) {
-      cmdLine.push('--config');
-      cmdLine.push(program.config);
-    }
-    if (program.profile) {
-      cmdLine.push('--profile');
-      cmdLine.push(program.profile);
-    }
-
-    console.log('\n===============================================================================');
-    console.log('| Execute:');
-    console.log('| $', cmdLine.join('\n|\t'));
-    console.log('===============================================================================\n');
-    exec(cmdLine.join(' '), {async:true}).stdout.on('data', function(data) {
-        if(data.indexOf('Starting factory') !== -1) {
-            // Open browser if asked
-            if (!program.serverOnly) {
-                require('open')('http://localhost:' + program.port);
-            }
+  console.log(program);
+    if (program.addDataset) {
+        if (!program.data) {
+            console.log('Adding data requires specifying the data directory to use with -d or --data');
+            process.exit(1);
         }
-    });
+        if (!program.description) {
+            console.log('Adding data requires specifying the description of the dataset with --description');
+        }
+        const addDatasetCmdLine = [ pvPythonExecs[0], '-dr',
+            path.normalize(path.join(__dirname, '../server/add_dataset.py')),
+            '--data-dir', program.data, '--file', program.addDataset, '--description',
+            program.description
+        ];
+        if (program.autoApply) {
+          addDatasetCmdLine.push('--autoApply');
+        }
+
+        console.log('\n===============================================================================');
+        console.log('| Execute:');
+        console.log('| $', addDatasetCmdLine.join('\n|\t'));
+        console.log('===============================================================================\n');
+        exec(addDatasetCmdLine.join(' '), {async:true}).stdout.on('data', function(data) {
+            if(data.indexOf('Starting factory') !== -1) {
+                // Open browser if asked
+                if (!program.serverOnly) {
+                    require('open')('http://localhost:' + program.port);
+                }
+            }
+        });
+    } else {
+        const cmdLine = [
+            pvPythonExecs[0], '-dr',
+            path.normalize(path.join(__dirname, '../server/pvw-light-viz.py')),
+            '--content', path.normalize(path.join(__dirname, '../dist')),
+            '--port', program.port,
+        ];
+        if (program.data) {
+          cmdLine.push('--data');
+          cmdLine.push(program.data);
+        }
+        if (program.config) {
+          cmdLine.push('--config');
+          cmdLine.push(program.config);
+        }
+        if (program.profile) {
+          cmdLine.push('--profile');
+          cmdLine.push(program.profile);
+        }
+
+        console.log('\n===============================================================================');
+        console.log('| Execute:');
+        console.log('| $', cmdLine.join('\n|\t'));
+        console.log('===============================================================================\n');
+        exec(cmdLine.join(' '), {async:true}).stdout.on('data', function(data) {
+            if(data.indexOf('Starting factory') !== -1) {
+                // Open browser if asked
+                if (!program.serverOnly) {
+                    require('open')('http://localhost:' + program.port);
+                }
+            }
+        });
+    }
 }
 
 
---
+++ server/add_dataset.py
@@ -0,0 +1,141 @@
+import os
+import json
+import shutil
+
+from paraview import simple
+
+def humanReadableSize(size):
+    suffixes = ['', 'K', 'M', 'G', 'T', 'P'] # We don't support data files in exobytes
+    count = 0
+    remaining = size
+    while remaining > 1024:
+        remaining = remaining / 1024
+        count += 1
+    return '%d%s' % (remaining, suffixes[count])
+
+def getArrayInformation(array, location):
+    rangeComponent = 0
+    if array.GetNumberOfComponents() > 1:
+        rangeComponent = -1
+    result = {
+        'name': array.GetName(),
+        'label': array.GetName(),
+        'dimension': array.GetNumberOfComponents(),
+        'range': [ x for x in array.GetRange(rangeComponent)],
+        'location': location,
+    }
+    return result
+
+def updateArrayBoundsForTimestep(array, arrayInfoMap):
+    info = arrayInfoMap[array.GetName()]
+    rangeComponent = 0
+    if array.GetNumberOfComponents() > 1:
+        rangeComponent = -1
+    newRange = array.GetRange(rangeComponent)
+    info['range'] = [ min(newRange[0], info['range'][0]),
+                      max(newRange[1], info['range'][1])]
+
+def loadArrayData(data, location, arrayMap):
+    for i in range(data.GetNumberOfArrays()):
+        array = data.GetArray(i)
+        if array:
+            if array.GetName() in arrayMap:
+                updateArrayBoundsForTimestep(array, arrayMap)
+            else:
+                arrayMap[array.GetName()] = getArrayInformation(array, location)
+
+def loadArrayDataMultiBlock(dataset, pointArrayMap, cellArrayMap):
+    if dataset is None:
+        return
+    if dataset.GetClassName() == "vtkMultiBlockDataSet":
+        for i in range(dataset.GetNumberOfBlocks()):
+            loadArrayDataMultiBlock(dataset.GetBlock(i), pointArrayMap, cellArrayMap)
+    else:
+        loadArrayData(dataset.GetPointData(), 'Point', pointArrayMap)
+        loadArrayData(dataset.GetCellData(), 'Cell', cellArrayMap)
+
+def unionBounds(b1, b2):
+    if b1 is None:
+        return b2
+    if b2 is None:
+        return b1
+    return [min(b1[0], b2[0]), max(b1[1], b2[1]),
+            min(b1[2], b2[2]), max(b1[3], b2[3]),
+            min(b1[4], b2[4]), max(b1[5], b2[5])]
+
+def getBounds(ds):
+    if ds is None:
+        return None
+    bounds = None
+    if ds.GetClassName() == "vtkMultiBlockDataSet":
+        for i in range(ds.GetNumberOfBlocks()):
+            if bounds is None:
+                bounds = getBounds(ds.GetBlock(i))
+            else:
+                bounds = unionBounds(bounds, getBounds(ds.GetBlock(i)))
+    else:
+        bounds = ds.GetBounds()
+    return bounds
+
+def importDataset(dataDir, datafile, description, autoApply=True):
+    if not os.path.exists(datafile):
+        print "Data file \"%s\" does not exist" % datafile
+        return
+    basename = os.path.basename(datafile)
+    filedir = os.path.join(dataDir, basename)
+    os.mkdir(filedir)
+    shutil.copyfile(datafile, os.path.join(filedir, basename))
+    result = {
+        'name': basename,
+        'size': humanReadableSize(os.path.getsize(datafile)),
+        'description': description,
+        'thumbnails': [],
+        'autoApply': autoApply,
+        'data': {
+            'file': basename,
+            'bounds': None,
+            'arrays': [],
+            'time': [],
+        },
+    }
+    reader = simple.OpenDataFile(datafile)
+    ds = reader.GetClientSideObject().GetOutputDataObject(0)
+    pointArrayMap = {}
+    cellArrayMap = {}
+    loadArrayDataMultiBlock(ds, pointArrayMap, cellArrayMap)
+    bounds = getBounds(ds)
+
+    if 'TimestepValues' in reader.ListProperties() and len(reader.TimestepValues) > 0:
+        for idx in range(len(reader.TimestepValues)):
+            t = reader.TimestepValues[idx]
+            result['data']['time'].append({ 'idx': idx, 'value': t })
+            reader.UpdatePipeline(t)
+            ds = reader.GetClientSideObject().GetOutputDataObject(0)
+            loadArrayDataMultiBlock(ds, pointArrayMap, cellArrayMap)
+            newBounds = getBounds(ds)
+            bounds = unionBounds(bounds, newBounds)
+
+    result['data']['arrays'] = pointArrayMap.values() + cellArrayMap.values()
+
+    result['data']['bounds'] = bounds
+
+    rep = simple.Show(reader)
+    rep.Visibility = 1
+    view = simple.Render()
+    view.ViewSize = [400, 400]
+    tnpath = os.path.join(filedir, 'thumbnail0.png')
+    simple.SaveScreenshot(tnpath, view)
+    result['thumbnails'].append('thumbnail0.png')
+
+    with open(os.path.join(filedir, 'index.json'), 'w') as fp:
+        json.dump(result, fp)
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='A script to import data files into lightviz')
+    parser.add_argument('--data-dir', help='Lightviz data directory to import to', dest='dataDir', required=True)
+    parser.add_argument('--file', help='file to import', dest='file', required=True)
+    parser.add_argument('--description', help='description of the dataset', dest='description', required=True)
+    parser.add_argument('--autoApply', action='store_true', default=False, help='Add this option to enable automatic Apply calls for the dataset', dest='autoApply')
+    args = parser.parse_args()
+    importDataset(args.dataDir, args.file, args.description, args.autoApply)

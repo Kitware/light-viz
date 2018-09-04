cc87dafc93a41c834c0da56b8255ea8b0103b924
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-09-04 13:33:33 -0600

    fix(Sphere): Improve sphere module

--- src/modules/Sphere/script.js
+++ src/modules/Sphere/script.js
@@ -8,33 +8,43 @@ export default generateComponentWithServerBinding(
   {
     startPhi: {
       name: 'StartPhi',
-      autoApply: true,
+      autoApply: false,
       default: 0,
     },
     endPhi: {
       name: 'EndPhi',
-      autoApply: true,
+      autoApply: false,
       default: 180,
     },
     phiResolution: {
       name: 'PhiResolution',
-      autoApply: true,
+      autoApply: false,
       default: 8,
     },
     startTheta: {
       name: 'StartTheta',
-      autoApply: true,
+      autoApply: false,
       default: 0,
     },
     endTheta: {
       name: 'EndTheta',
-      autoApply: true,
+      autoApply: false,
       default: 360,
     },
     thetaResolution: {
       name: 'ThetaResolution',
-      autoApply: true,
+      autoApply: false,
       default: 8,
+    },
+    radius: {
+      name: 'Radius',
+      autoApply: false,
+      default: 0.5,
+    },
+    center: {
+      name: 'Center',
+      autoApply: false,
+      default: [0, 0, 0],
     },
   },
   {
@@ -43,6 +53,23 @@ export default generateComponentWithServerBinding(
       return {
         module,
         color: 'grey darken-2',
+        domains: {
+          xCenter: {
+            min: -5,
+            max: 5,
+            step: 1,
+          },
+          yCenter: {
+            min: -5,
+            max: 5,
+            step: 1,
+          },
+          zCenter: {
+            min: -5,
+            max: 5,
+            step: 1,
+          },
+        },
       };
     },
     computed: {
@@ -62,6 +89,48 @@ export default generateComponentWithServerBinding(
         set(value) {
           this.startTheta = Number(value[0]);
           this.endTheta = Number(value[1]);
+        },
+      },
+      xCenter: {
+        get() {
+          // register dependency
+          this.mtime; // eslint-disable-line
+          return this.center[0];
+        },
+        set(value) {
+          this.mtime++;
+          const newCenter = this.center.slice();
+          newCenter[0] = value;
+          this.center = newCenter.map(Number);
+          this.$forceUpdate();
+        },
+      },
+      yCenter: {
+        get() {
+          // register dependency
+          this.mtime; // eslint-disable-line
+          return this.center[1];
+        },
+        set(value) {
+          this.mtime++;
+          const newCenter = this.center.slice();
+          newCenter[1] = value;
+          this.center = newCenter.map(Number);
+          this.$forceUpdate();
+        },
+      },
+      zCenter: {
+        get() {
+          // register dependency
+          this.mtime; // eslint-disable-line
+          return this.center[2];
+        },
+        set(value) {
+          this.mtime++;
+          const newCenter = this.center.slice();
+          newCenter[2] = value;
+          this.center = newCenter;
+          this.$forceUpdate();
         },
       },
     },
